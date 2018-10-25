81997d006c5c66cfe3fbcd820620e4830e97245f
maks3217 <maks3217@wp.pl>
2018-10-25 21:06:25 +0200

    The main commit

---
+++ Assets/scripts/GUI/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    public  Button startButton;
+    public  Canvas menuSelectLvl;
+    private Canvas menuUI;
+
+    public Canvas background;
+
+    void Start () {
+        menuUI = (Canvas)GetComponent<Canvas>();
+        menuSelectLvl = (Canvas)menuSelectLvl.GetComponent<Canvas>();
+
+        background = background.GetComponent<Canvas>();
+
+        startButton = startButton.GetComponent<Button>();
+        startButton.onClick.AddListener(SelectLevelMenu);
+        background.enabled = true;
+        menuUI.enabled = true;
+        menuSelectLvl.enabled = false;
+
+    }
+
+    void Update () {
+	}
+
+    public void SelectLevelMenu()
+    {
+        menuUI.enabled = false;
+        menuSelectLvl.enabled = true;
+    }
+
+}
---
+++ Assets/scripts/GUI/SelectLvlMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SelectLvlMenu : MonoBehaviour {
+
+    public GameObject prefabButton;
+    public RectTransform ParentPanel;
+    public Canvas background;
+
+    public int lvlCount = 10;
+
+    private Canvas thisMenu;
+
+    void Start () {
+        thisMenu = (Canvas)GetComponent<Canvas>();
+        background = background.GetComponent<Canvas>();
+
+        for (int i = 1; i <= lvlCount; i++)
+        {
+            GameObject goButton = (GameObject)Instantiate(prefabButton);
+            goButton.name = "Button lvl " + i;
+            goButton.GetComponentInChildren<Text>().text = ""+i;
+            goButton.transform.SetParent(ParentPanel, false);
+
+            Button tempButton = goButton.GetComponent<Button>();
+            int tempInt = i;
+
+            tempButton.onClick.AddListener(() => ButtonClicked(tempInt));
+        }
+
+    }
+
+    private void ButtonClicked(int id)
+    {
+        thisMenu.enabled = false;
+        background.enabled = false;
+
+        SceneManager.LoadScene(id);
+    }
+
+    void Update () {
+		
+	}
+}
